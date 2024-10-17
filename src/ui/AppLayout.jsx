01f38dd0ab41b8ee13663f6b27abd8e9bb85ae01
predrag jandric <predrag.jandric.bg@gmail.com>
2024-10-18 01:35:52 +0200

    will ADD: data loading using render as you fetch strategy using react router loader feature inside Menu.jsx

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,20 +1,20 @@
-import { Outlet } from "react-router-dom"
-import CartOverview from "../features/cart/CartOverview"
-import Header from "./Header"
+import { Outlet } from "react-router-dom";
+import CartOverview from "../features/cart/CartOverview";
+import Header from "./Header";
 
 function AppLayout() {
-    return (
-        <div>
-            <Header/>
+  return (
+    <div>
+      <Header />
 
-            <main>
-                <h1>Content</h1>
-                <Outlet/>
-            </main>
+      <main>
+        <h1>Content</h1>
+        <Outlet />
+      </main>
 
-            <CartOverview />
-        </div>
-    )
+      <CartOverview />
+    </div>
+  );
 }
 
-export default AppLayout
+export default AppLayout;
