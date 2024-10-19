f9337433b89907743ef8ca98d8871a2f394e066f
predrag jandric <predrag.jandric.bg@gmail.com>
2024-10-20 01:18:10 +0200

    will ADD: style for buttons and REFACTOR for buttons into separate components, so that they are reusable. will also ADD: styling for the loader

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -3,20 +3,26 @@ import { useNavigate } from "react-router-dom";
 
 function SearchOrder() {
   const [query, setQuery] = useState("");
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-function handleSubmit(e) {
-    e.preventDefault()
+  function handleSubmit(e) {
+    e.preventDefault();
 
-    if(!query) return
-    navigate(`/order/${query}`)
-    setQuery("")
-}
+    if (!query) return;
+    navigate(`/order/${query}`);
+    setQuery("");
+  }
 
   return (
-  <form onSubmit={handleSubmit}>
-  <input placeholder="Search order #" value={query} onChange={(e)=> setQuery(e.target.value)} />
-  </form>
-)}
+    <form onSubmit={handleSubmit}>
+      <input
+        className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm placeholder:text-stone-400 transition-all sm:w-64 sm:focus:w-72 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-opacity-50"
+        placeholder="Search order #"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </form>
+  );
+}
 
 export default SearchOrder;
