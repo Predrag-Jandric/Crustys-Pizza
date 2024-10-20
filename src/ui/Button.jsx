4740ede77aea6fcc80acfc5c37b90d8d9ba4bb19
predrag jandric <predrag.jandric.bg@gmail.com>
2024-10-20 22:56:17 +0200

    will ADD: styling for Cart.jsx and CartItem.jsx

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -8,7 +8,8 @@ function Button({ children, disabled, to, type }) {
 
   const styles = {
     primary: base + " px-4 py-3 sm:px-6 md:py-4",
-    small: base + " px-4 py-2 sm:px-5 md:py-2.5 text-xs"
+    small: base + " px-4 py-2 sm:px-5 md:py-2.5 text-xs",
+    secondary: " inline-block rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors focus:text-stone-800 hover:text-stone-800 hover:bg-stone-300 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 sm:px-6 md:py-3.5"
   };
 
   if (to)
