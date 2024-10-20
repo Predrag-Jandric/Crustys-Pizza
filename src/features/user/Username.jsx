00a79c7c731acd46ef27ed7e150edc69826b57a6
predrag jandric <predrag.jandric.bg@gmail.com>
2024-10-21 01:45:06 +0200

    will ADD: redux toolkit global state management system to the app, and will connect it and configure it

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,7 +1,13 @@
+import { useSelector } from "react-redux"
+
 function Username() {
+const username = useSelector(state=>state.user.username)
+
+if(!username) return null
+
     return (
         <p className="hidden md:block text-sm font-semibold">
-            Peter
+            {username}
         </p>
     )
 }
