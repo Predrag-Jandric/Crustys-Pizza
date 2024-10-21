418dc0314c1fd56fce9dab96e82c5c7dedc91bf3
predrag jandric <predrag.jandric.bg@gmail.com>
2024-10-21 23:54:33 +0200

    will ADD: data of selected number of items and their total price in the menu route, displayed in the CartOverview.jsx at the bottom of the screen

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,16 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  //   cart: [],
-  cart: [
-    {
-      pizzaId: 14,
-      name: "Mediterranean",
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-  ],
+    cart: [],
+//   cart: [
+//     {
+//       pizzaId: 14,
+//       name: "Mediterranean",
+//       quantity: 2,
+//       unitPrice: 16,
+//       totalPrice: 32,
+//     },
+//   ],
 };
 
 const cartSlice = createSlice({
@@ -39,6 +39,17 @@ const cartSlice = createSlice({
   },
 });
 
-export const {addItem, deleteItem, increaseItemQuantity, decreaseItemQuantity, clearCart} = cartSlice.actions
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
 
-export default cartSlice.reducer
+export const getTotalCartPrice = (state)=> state.cart.cart.reduce((sum, item)=> sum + item.totalPrice, 0)
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
