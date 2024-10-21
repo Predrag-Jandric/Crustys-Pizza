3d9c4e8e9176ffe476c13339d99d2061cab89290
predrag jandric <predrag.jandric.bg@gmail.com>
2024-10-22 01:24:29 +0200

    will ADD: functionality of clearing the cart by pressing the button clear cart in cart route, and conditionally rendered content inside cart

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,39 +1,24 @@
-import { Link } from "react-router-dom";
 import LinkButton from "../../ui/LinkButton";
 import Button from "../../ui/Button";
 import CartItem from "./CartItem";
-import { useSelector } from "react-redux";
-
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: "Mediterranean",
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: "Vegetale",
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: "Spinach and Mushroom",
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart, getCart } from "./cartSlice";
+import { getUsername } from "../user/userSlice";
+import EmptyCart from "./EmptyCart";
 
 function Cart() {
-  const cart = fakeCart;
-  const username = useSelector((state) => state.user.username);
+  const username = useSelector(getUsername);
+  const cart = useSelector(getCart);
+  const dispatch = useDispatch();
+
+  function handleClearCart() {
+    dispatch(clearCart());
+  }
+
+  if (!cart.length) return <EmptyCart />;
 
   return (
-    <div className="md:w-[30rem] px-4 py-3">
+    <div className="px-4 py-3 md:w-[30rem]">
       <LinkButton to={"/menu"}>&larr; Back to menu</LinkButton>
 
       <h2 className="mt-7 text-xl font-semibold">Your cart, {username}</h2>
@@ -49,8 +34,9 @@ function Cart() {
           Order pizzas
         </Button>
 
-        <Button type="secondary">Clear Cart</Button>
-
+        <Button onClick={handleClearCart} type="secondary">
+          Clear Cart
+        </Button>
       </div>
     </div>
   );
