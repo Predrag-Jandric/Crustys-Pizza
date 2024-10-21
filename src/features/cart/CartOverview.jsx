b32ee8b1f3840275fda25a2b05650c80df6deb5c
predrag jandric <predrag.jandric.bg@gmail.com>
2024-10-22 01:36:10 +0200

    will ADD: style to the EmptyCart.jsx component which is conditionally rendered and fix to cart overview to a fixed position so its always visible

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -5,13 +5,13 @@ import { formatCurrency } from "../../utils/helpers";
 
 function CartOverview() {
   const totalCartQuantity = useSelector(getTotalCartQuantity);
-  const totalCartPrice = useSelector(getTotalCartPrice)
+  const totalCartPrice = useSelector(getTotalCartPrice);
 
   // hides the cart info at the bottom of the screen if we have zero items selected
-// if(!totalCartQuantity) return null
+  // if(!totalCartQuantity) return null
 
   return (
-    <div className="flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
+    <div className="fixed bottom-0 left-0 right-0 z-50 flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
         <span>{totalCartQuantity} pizzas</span>
         <span>{formatCurrency(totalCartPrice)}</span>
